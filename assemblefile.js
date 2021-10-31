98d8ba83782c811cfdb37e983a20863f8e4d79aa
Osman Nuri Okumus <onokmus@gmail.com>
2021-10-31 19:06:57 +0300

    remove different folder build

--- assemblefile.js
+++ assemblefile.js
@@ -1,4 +1,5 @@
 'use strict';
+
 var assemble = require('assemble');
 var extname = require('gulp-extname');
 var path = require('path');
@@ -7,23 +8,9 @@ var helpers = require('handlebars-helpers');
 var prettify = require('gulp-prettify');
 var pkg = require('./package.json');
 var bower = require('./bower.json');
-
-var vnd = 'docs/assets/vendor';
-
-var banner = [
-  '/**',
-  ' * <%= pkg.name %> - <%= pkg.description %>',
-  ' * @version v<%= pkg.version %>',
-  ' * @author <%= pkg.author %>',
-  ' */',
-  ''
-].join('\n');
-
 var app = assemble();
-app.create('home');
 
-
-app.task('init', function(cb) {
+app.task('default', function() {
   var demo = app.options.demo;
   var projectName = 'Chaldene';
   var imgExt = 'svg';
@@ -38,7 +25,7 @@ app.task('init', function(cb) {
   app.data('projectName', projectName);
   app.data('prod', app.options.prod);
   app.option('layout', 'first');
-  app.option('assets', '../assets/');
+  app.option('assets', 'assets/');
   app.helper('math', helpers.math());
   app.helper('number', helpers.number());
   app.helper('date', helpers.date());
@@ -49,15 +36,9 @@ app.task('init', function(cb) {
   app.data(['./templates/data/**/*.json']);
   app.data('pkg', pkg);
   app.data('bower', bower);
-  app.layouts(path.join(__dirname, './templates/layouts/**/*.hbs'));
-  app.partials(path.join(__dirname, './templates/includes/**/*.hbs'));
-  app.pages(path.join(__dirname, './templates/content/**/*.hbs'));
-  app.home(path.join(__dirname, './home.hbs'));
-  cb();
-});
-
-
-function contentBuild(dest) {
+  app.layouts('templates/layouts/**/*.hbs');
+  app.partials('templates/includes/**/*.hbs');
+  app.pages('templates/content/**/*.hbs');
   return app.toStream('pages')
     .pipe(app.renderFile())
     .on('err', console.error)
@@ -68,78 +49,7 @@ function contentBuild(dest) {
       extra_liners: ['head', 'body']
     }))
     .pipe(extname())
-    .pipe(app.dest(path.join(__dirname, './docs/' + dest)));
-}
-
-app.task('home', ['init'], function() {
-  var homeImageExt = 'png';
-  
-  app.data('homeImageExt', homeImageExt);
-
-  return app.toStream('home')
-    .pipe(app.renderFile())
-    .on('err', console.error)
-    .pipe(prettify({
-      indent_inner_html: false,
-      preserve_newlines: true,
-      end_with_newline: true,
-      extra_liners: ['head', 'body']
-    }))
-    .pipe(extname())
-    .pipe(rename('index.html'))
-    .pipe(app.dest(path.join(__dirname, './docs')));
+    .pipe(app.dest('docs'));
 });
-
-app.task('content-first', ['init'], function() {
-  app.option('layout', 'first');
-  app.data('lay', 'first');
-  return contentBuild('first');
-});
-
-app.task('content-second', ['init'], function() {
-  app.option('layout', 'second');
-  app.data('lay', 'second');
-  return contentBuild('second');
-});
-
-app.task('content-third', ['init'], function() {
-  app.option('layout', 'third');
-  app.data('lay', 'third');
-  app.data({
-    topNav: true
-  });
-  return contentBuild('third');
-});
-
-app.task('content-firstrtl', ['init'], function() {
-  app.option('layout', 'first');
-  app.data('lay', 'first');
-  app.data('rtl', true);
-  return contentBuild('firstrtl');
-});
-
-app.task('content-secondrtl', ['init'], function() {
-  app.option('layout', 'second');
-  app.data('lay', 'second');
-  app.data('rtl', true);
-  return contentBuild('secondrtl');
-});
-
-app.task('content-thirdrtl', ['init'], function() {
-  app.option('layout', 'third');
-  app.data('lay', 'third');
-  app.data({
-    topNav: true
-  });
-  app.data('rtl', true);
-  return contentBuild('thirdrtl');
-});
-
-app.task('content', ['content-*']);
-
-app.task('rtl', ['content-firstrtl', 'content-secondrtl', 'content-thirdrtl'], function(){});
-app.task('ltr', ['content-first', 'content-second', 'content-third'], function(){});
-
-app.task('default', ['content-first'], function() {});
 
 module.exports = app;
