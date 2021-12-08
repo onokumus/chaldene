f4953d2671db6ca78935b1f6db887ea7a22ec6f2
Osman Nuri Okumus <onokumus@gmail.com>
2021-12-08 13:25:33 +0300

    remove inview & animate for performance

--- docs/assets/js/pagejs/ui-icon-awesome.js
+++ docs/assets/js/pagejs/ui-icon-awesome.js
@@ -1,18 +1,9 @@
 $(document).ready(function () {
 
-
-  $('.inview').each(function () {
-    inView('.inview').on('enter', function (el) {
-      $(el).addClass('animated fadeIn').removeClass('fadeOut');
-    }).on('exit', function (el) {
-      $(el).addClass('fadeOut').removeClass('fadeIn');
-    });
-  });
-
   var clipboard = new ClipboardJS('.btn');
   clipboard.on('success', function(e) {
     noty({
-      text: 'Font classes <strong class="text-peter-river">('+e.text+')</strong> copied to clipboard',
+      text: 'Font classes <strong class="text-primary">('+e.text+')</strong> copied to clipboard',
       layout: 'topRight',
       type: 'success',
       theme: 'relax',
--- docs/assets/js/pagejs/ui-icon-glyphicon.js
+++ docs/assets/js/pagejs/ui-icon-glyphicon.js
@@ -1,18 +1,9 @@
 $(document).ready(function () {
 
-  $('.inview').each(function () {
-    inView('.inview').on('enter', function (el) {
-      $(el).addClass('animated fadeIn').removeClass('fadeOut');
-    }).on('exit', function (el) {
-      $(el).addClass('fadeOut').removeClass('fadeIn');
-    });
-  });
-
-
   var clipboard = new ClipboardJS('.btn');
   clipboard.on('success', function(e) {
     noty({
-      text: 'Font classes <strong class="text-alizarin">('+e.text+')</strong> copied to clipboard',
+      text: 'Font classes <strong class="text-danger">('+e.text+')</strong> copied to clipboard',
       layout: 'topRight',
       type: 'success',
       theme: 'relax',
