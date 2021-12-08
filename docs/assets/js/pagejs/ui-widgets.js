6e6ba771d06b0254bd6842784c64f48f9438945a
Osman Nuri Okumus <onokumus@gmail.com>
2021-12-08 11:08:55 +0300

    change date

--- docs/assets/js/pagejs/ui-widgets.js
+++ docs/assets/js/pagejs/ui-widgets.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-  $.fn.countdown && $('#countdown1').countdown('2020/10/10', function (event) {
+  $.fn.countdown && $('#countdown1').countdown('2023/04/23', function (event) {
     var $this = $(this).html(event.strftime('' + '<span>%w</span> weeks ' + '<span>%d</span> days ' + '<span>%H</span> hr ' + '<span>%M</span> min ' + '<span class="text-orange f-b animated fadeInUpBig">%S</span> sec'));
   });
 
