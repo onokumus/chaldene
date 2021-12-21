662cbf43c4bee08bb65593368d1a3358f208e2ea
Osman Nuri Okumus <onokumus@gmail.com>
2021-12-21 09:50:57 +0300

    remove less files and modules

--- gulpfile.js
+++ gulpfile.js
@@ -8,12 +8,8 @@ const {
 } = require('gulp');
 
 const header = require('gulp-header');
-const less = require('gulp-less');
-const autoprefixer = require('gulp-autoprefixer');
-const cssnano = require('gulp-cssnano');
 const rtlcss = require('gulp-rtlcss');
 const rename = require('gulp-rename');
-const concat = require('gulp-concat');
 const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
 const pkg = require('./package.json');
@@ -27,31 +23,9 @@ const banner = [
   ''
 ].join('\n');
 
-const vnd = 'docs/assets/vendor';
-
-
-function cssltr() {
-  return src(['./source/less/chaldene.less', './less/theme-*.less'])
-    .pipe(less())
-    .pipe(autoprefixer())
-    .pipe(header(banner, {
-      pkg
-    }))
-    .pipe(dest('docs/assets/css'))
-    .pipe(cssnano())
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(header(banner, {
-      pkg
-    }))
-    .pipe(dest('docs/assets/css'));
-}
 
 function cssrtl() {
-  return src(['./source/less/chaldene.less', './less/theme-*.less'])
-    .pipe(less())
-    .pipe(autoprefixer())
+  return src(['./docs/assets/css/chl.css'])
     .pipe(rtlcss())
     .pipe(header(banner, {
       pkg
@@ -60,7 +34,6 @@ function cssrtl() {
       suffix: '-rtl'
     }))
     .pipe(dest('docs/assets/css'))
-    .pipe(cssnano())
     .pipe(rename({
       suffix: '.min'
     }))
@@ -68,20 +41,6 @@ function cssrtl() {
       pkg
     }))
     .pipe(dest('docs/assets/css'));
-}
-
-function bsrtl() {
-  return src('docs/assets/vendor/bootstrap/css/bootstrap.css')
-    .pipe(rtlcss())
-    .pipe(rename('bootstrap-rtl.css'))
-    .pipe(dest('docs/assets/vendor/bootstrap/css/'));
-}
-
-function bsrtlmin() {
-  return src('docs/assets/vendor/bootstrap/css/bootstrap.min.css')
-    .pipe(rtlcss())
-    .pipe(rename('bootstrap-rtl.min.css'))
-    .pipe(dest('docs/assets/vendor/bootstrap/css/'));
 }
 
 function js() {
@@ -96,16 +55,7 @@ function js() {
     .pipe(dest('docs/assets/js'));
 }
 
-const css = parallel(cssltr, cssrtl);
-const bs = parallel(bsrtl, bsrtlmin);
-
-exports.cssltr = cssltr;
 exports.cssrtl = cssrtl;
-exports.bsrtl = bsrtl;
-exports.bsrtlmin = bsrtlmin;
-exports.css = css;
 exports.js = js;
 
-exports.rtl = parallel(cssrtl, bsrtl, bsrtlmin);
-
-exports.default = series(cssltr, js);
+exports.default = series(cssrtl, js);
