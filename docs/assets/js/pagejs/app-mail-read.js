d7a1e17f01aa4f8d88f28dddb186a6a5f5d730ad
Osman Nuri Okumus <onokumus@gmail.com>
2022-06-30 18:35:56 +0300

    remove lightgallery

--- docs/assets/js/pagejs/app-mail-read.js
+++ docs/assets/js/pagejs/app-mail-read.js
@@ -1,17 +1,9 @@
 $(document).ready(function () {
 
-
-  $("#lightgallery").lightGallery({
-    selector: ".thumbnail",
-    thumbnail: true
-  });
-
   if (window.insignia !== undefined) {
     insignia(document.getElementById("insignia"), {deletion: true});
   }
   new Quill("#exampleTextAreaMessage", { theme: "bubble" });
-
-
 
 
 });
